8189dd9b88b771a6b87c04a9415515215c753343
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2021-02-12 23:38:45 -0800

    second commit

--- aerodynamic-control-module-dev/Debug/Core/Src/GopherCAN_ring_buffer.d
+++
@@ -1,9 +0,0 @@
-Core/Src/GopherCAN_ring_buffer.o: ../Core/Src/GopherCAN_ring_buffer.c \
- ../Core/Inc/GopherCAN_ring_buffer.h ../Core/Inc/GopherCAN_structs.h \
- ../Core/Inc/base_types.h
-
-../Core/Inc/GopherCAN_ring_buffer.h:
-
-../Core/Inc/GopherCAN_structs.h:
-
-../Core/Inc/base_types.h:
